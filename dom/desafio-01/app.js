ba4894fbbf6a1b0d61b89320c38f0713cfba0ba2
Ronaldo Paulo da Silva Filho <ronaldopsilvafilho@gmail.com>
2022-02-23 21:10:19 -0300

    Atualizando o dia at o desafio na pasta introducao

---
+++ dom/desafio-01/app.js
@@ -0,0 +1,14 @@
+new Vue({
+    el: "#desafio",
+    data:{
+        Nome: "Ronaldo", Idade: 30, imagem: "http://files.cod3r.com.br/curso-vue/vue.jpg" 
+    },
+    methods:{
+        multiplicador(){
+            return this.Idade * 3
+        },
+        random(){
+            return Math.random()  
+        },
+    }
+})
